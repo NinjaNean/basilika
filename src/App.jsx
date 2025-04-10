10c86d76f4690191437d656f3c891cde76f63c5e
Linnéa Nilsson <linnea.nilsson97@hotmail.com>
2025-04-10 19:09:04 +0200

    basic setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
+import { NavLink, Outlet } from "react-router";
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <header>
+        <NavLink to={"/"}>Home</NavLink>
+        <NavLink to={"/contact"}>Contact</NavLink>
+        <NavLink to={"/menu"}>Menu</NavLink>
+        <NavLink to={"/signIn"}>Sign in</NavLink>
+      </header>
+      <main>
+        <Outlet />
+      </main>
+      <footer></footer>
+    </>
+  );
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,40 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { createHashRouter, RouterProvider } from "react-router";
+import Home from "./Pages/Home.jsx";
+import Contact from "./Pages/Contact.jsx";
+import Menu from "./Pages/Menu.jsx";
+import SignIn from "./Pages/SignIn.jsx";
 
-createRoot(document.getElementById('root')).render(
+let router = createHashRouter([
+  {
+    path: "/",
+    Component: App,
+    children: [
+      {
+        index: true,
+        Component: Home,
+      },
+      {
+        path: "/contact",
+        Component: Contact,
+      },
+      {
+        path: "/menu",
+        Component: Menu,
+      },
+      {
+        path: "/signin",
+        Component: SignIn,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
