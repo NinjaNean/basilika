063476ad2bcb7cb7ed37c5528694e9227c361561
Sara Serti <saaris1985@gmail.com>
2025-04-13 20:34:38 +0200

    mediaQ

--- src/Component/Footer.jsx
+++ src/Component/Footer.jsx
@@ -3,7 +3,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTwitter } from '@fortawesome/free-brands-svg-icons';
 import { faFacebook } from '@fortawesome/free-brands-svg-icons';
 import { faInstagram } from '@fortawesome/free-brands-svg-icons';   
-import { smallMap } from './assets/smallMap.jpg';
+import map from '../assets/map.png';
 
 
 
@@ -12,33 +12,34 @@ const Footer = () => {
         <footer className="footer">
             <div className="footer-container">
                 <div className="footer-heading1">
-                    <h3>Basil</h3>
-                    <p>Besök oss i Göteborg och<br/> smaka på våra rätter med<br/> ursprung i det asisatiaska köket</p>
+                     <h4 className='basil-header'>Basil</h4>    
+                    <p className='footer-text'>Besök oss i Göteborg och<br/> smaka på våra rätter med<br/> ursprung i det asiatiska köket</p>
                 </div>
 
                 <div className="footer-heading1">
                     <h4>Kontakt</h4>
-                    <p>basil@gmail.com <br />031-123456</p>
+                    <p className='footer-text'> basil@gmail.com <br />031-123456</p>
                         <section className='footer-font'>
-                        <FontAwesomeIcon icon={faTwitter} />
-                        <FontAwesomeIcon icon={faFacebook} />
-                        <FontAwesomeIcon icon={faInstagram} />  
+                        <FontAwesomeIcon icon={faTwitter} className='fonts'/>
+                        <FontAwesomeIcon icon={faFacebook} className='fonts'/>
+                        <FontAwesomeIcon icon={faInstagram} className='fonts'/>  
                         </section> 
                 </div>
 
                 <div className="footer-heading1">
                     <h4>Adress</h4>
-                    <p>Redegatan 2<br/>42155 Göteborg </p>
+                    <p className='footer-text'>Redegatan 2<br/>42155 Göteborg </p>
                 </div>
 
                 <div className="footer-heading1">
                     <h4>Hitta oss</h4>
-                    <img src={smallMap} alt="small map" />
+                    <img className='small-map' src={map} alt="small map" />
                 </div>
-            </div>
+            
                 <section className='footer-source'>
-                    <p> Utvecklad och designad av Linnéa, Sally, Shweta, Malahat och Sara </p>
+                    <p className='source'> Utvecklad och designad av Linnéa, Sally, Shweta, Malahat och Sara </p>
                 </section>
+            </div>
         </footer>
     );
 }
