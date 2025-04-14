f29fb00be8f9b541b074a4108e605323656f850e
Linnéa Nilsson <linnea.nilsson97@hotmail.com>
2025-04-14 14:52:48 +0200

    renderat ut menyn + börjat styla
    Co-authored-by: slindgr7 <slindgr7@users.noreply.github.com>

--- src/Component/Menu/Menu.jsx
+++ src/Component/Menu/Menu.jsx
@@ -1,15 +1,27 @@
 import React from "react";
 import MenuItem from "./MenuItem";
 import infoImg from "../../assets/information-svg.svg";
+import { menuData } from "../../Data.js/menuData.js";
 
 function Menu() {
   return (
     <div className="menu">
-      <p>
+      <p className="allergen-info">
         <img src={infoImg} alt="information icon" />
         Allergi information
       </p>
-      <MenuItem />
+
+      {menuData.map((foodItem) => {
+        return (
+          <MenuItem
+            key={foodItem.id}
+            name={foodItem.name}
+            price={foodItem.price}
+            description={foodItem.description}
+            img={foodItem.img}
+          />
+        );
+      })}
     </div>
   );
 }
--- src/Component/Menu/MenuItem.jsx
+++ src/Component/Menu/MenuItem.jsx
@@ -3,23 +3,25 @@ import infoImg from "../../assets/information-svg.svg";
 import minusImg from "../../assets/minus-svg.svg";
 import plusImg from "../../assets/plus-svg.svg";
 
-function MenuItem() {
+function MenuItem({ name, price, description, img }) {
   return (
     <div className="menu-item">
-      <p>
-        Tempura räka <img src={infoImg} alt="information icon" />
-      </p>
-      <p>avokado, färskost, misomajonnäs, wasabikrisp</p>
-      <p>104:-</p>
-      <p>
-        <img src={plusImg} alt="plus icon" />
-        0
-        <img src={minusImg} alt="minus icon" />
-      </p>
-      <img
-        src="https://www.kikkoman.eu/fileadmin/_processed_/0/f/csm_1025-recipe-page-Spicy-tuna-and-salmon-rolls_desktop_b6172c0072.jpg"
-        alt="food picture"
-      />
+      <div>
+        <h2>
+          {name} <img src={infoImg} alt="information icon" />
+        </h2>
+        <p>{description}</p>
+      </div>
+
+      <div>
+        <p>{price}:-</p>
+        <div className="cart-buttons">
+          <img src={plusImg} alt="plus icon" />
+          <p>0</p>
+          <img src={minusImg} alt="minus icon" />
+        </div>
+      </div>
+      <img src={img} alt="food picture" />
     </div>
   );
 }
