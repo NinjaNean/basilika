b8d8e485a7657c1b04ed2e1cff8d26f03cf5eb42
Sara Serti <saaris1985@gmail.com>
2025-04-17 15:33:13 +0200

    change footer style

--- src/Component/footer/Footer.jsx
+++ src/Component/footer/Footer.jsx
@@ -13,15 +13,17 @@ const Footer = () => {
             <section className='footer-section'>
                 <div className="footer-heading1">
 
-                    <img src={smallLogo} alt="Logo" className="small-logo" />
-                    <p className='footer-text first'>Taste our dishes with origin in asian cuisine</p>
+                    <h4>バジル Basil</h4>
+                    <p className='footer-text'>Taste our dishes with origin in asian cuisine</p>
                 </div>
 
                 <div className="footer-heading1">
                     <h4>Contact</h4>
-                    <p className='footer-text second'> basil@gmail.se 031-123456</p>
+                    <p className='footer-text'> basil@gmail.se </p>
+                    <p className='footer-text'> 031-123456 </p>
                         <section className='footer-font'>
-                        <FontAwesomeIcon icon={faInstagram} className='fonts'/>
+                        
+                        <FontAwesomeIcon icon={faInstagram} fontSize={'20'}className='fonts'/>
                         <FontAwesomeIcon icon={faTwitter} className='fonts'/> 
                         <FontAwesomeIcon icon={faFacebook} className='fonts'/>
                         </section> 
@@ -31,12 +33,14 @@ const Footer = () => {
             <section className='footer-section'>   
                 <div className="footer-heading1">
                     <h4>Adress</h4>
-                    <p className='footer-text third'>Redegatan 2, 42155 Gothenburg </p>
+                    <p className='footer-text'>Redegatan 2 </p>
+                    <p className='footer-text'> 42155 Gothenburg</p>
                 </div>
 
                 <div className="footer-heading1">
                     <h4>Find us</h4>
-                    <img src={smallMap} alt="small map" />
+                    <p className='footer-text'>We are 100m from the city bus stop</p>
+                    {/* <img src={smallMap} alt="small map" /> */}
                 </div>
             </section>
             
