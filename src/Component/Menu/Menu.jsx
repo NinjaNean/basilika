c3da1fb59346d986f0f0a84e180a911e909dd182
Linnéa Nilsson <linnea.nilsson97@hotmail.com>
2025-04-24 15:48:09 +0200

    raderade kommentarer

--- src/Component/Menu/Menu.jsx
+++ src/Component/Menu/Menu.jsx
@@ -1,63 +1,3 @@
-/*import React, { useEffect, useState } from "react";
-import MenuItem from "./MenuItem.jsx";
-import useCartStore from "../../data/cartStore.js";
-import { menuData } from "../../data/menuData.js";
-
-function Menu() {
-  const { foodDataList, totalPrice, toggleItemActive } = useCartStore();
-  const categories = ["Sushi", "Dumplings", "Snacks", "Drycker"];
-  const [editClick, setEditClick] = useState(false);
-
-  const handleEditClick = () => {
-    setEditClick((prev) => !prev);
-  };
-  
-  const [menuItems, setMenuItems] = useState(menuData);
-
-
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-  useEffect(() => {
-  const loginStatus = localStorage.getItem("isLoggedIn");
-  setIsLoggedIn(loginStatus === "true");
-}, []);
-
-
-  //För felsökning
-  useEffect(() => {
-    console.log(totalPrice);
-  }, [totalPrice]);
-
-  return (
-    <div  className={`menu ${editClick ? "show-buttons" : ""}`}>
-       <div className="add-edit-buttons">
-       
-       <button 
-       className={`edit-item-button ${editClick ? "disabled" : ""}`} 
-       onClick={handleEditClick}> {editClick ? "Redigera" : "Redigera"}
-      </button>
-
-      <button className="add-item-button">Lägg till</button>
-      </div>
-
-      {categories.map((category) => (
-        <div key={category}>
-          <h1 id={category}>{category}</h1>
-          {foodDataList
-            .filter((item) => item.category === category)
-            .map((foodItem) => (
-              <MenuItem key={foodItem.id} foodItem={foodItem} 
-              active={foodItem.active}/>
-            ))}
-        </div>
-      ))}
-    </div>
-  );
-}
-
-export default Menu;
-*/
-
 import React, { useEffect, useState } from "react";
 import MenuItem from "./MenuItem.jsx";
 import useCartStore from "../../data/cartStore.js";
@@ -94,7 +34,9 @@ function Menu() {
             {editClick ? "Klart" : "Redigera"}
           </button>
           <button className="add-item-button">Lägg till</button>
-          <button className="logout-button" onClick={handleLogout}>Logga ut</button>
+          <button className="logout-button" onClick={handleLogout}>
+            Logga ut
+          </button>
         </div>
       )}
 
@@ -104,11 +46,7 @@ function Menu() {
           {foodDataList
             .filter((item) => item.category === category)
             .map((foodItem) => (
-              <MenuItem
-                key={foodItem.id}
-                foodItem={foodItem}
-                active={foodItem.active}
-              />
+              <MenuItem key={foodItem.id} foodItem={foodItem} active={foodItem.active} />
             ))}
         </div>
       ))}
