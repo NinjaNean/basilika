ca5f8b801393f0e035f3a11ec1524dc663a20704
Linnéa Nilsson <linnea.nilsson97@hotmail.com>
2025-04-25 10:39:20 +0200

    kommenterat ut

--- src/Pages/MenuPage/MenuPage.jsx
+++ src/Pages/MenuPage/MenuPage.jsx
@@ -5,22 +5,19 @@ import "./Menu.css";
 import heroImg from "../../assets/TemptAsian-Page-Header_1920x600-2.webp";
 import AddItem from "../../Component/add-item/AddItem.jsx";
 
-
-
-
-
 function MenuPage() {
-	const addFoodItem = useCartStore((state) => state.addFoodItem);
-  
-	const handleAddItem = (item) => {
-	  addFoodItem(item); 
-	};
-	
+  const addFoodItem = useCartStore((state) => state.addFoodItem);
+
+  const handleAddItem = (item) => {
+    addFoodItem(item);
+  };
+
   return (
-	
     <div className="menu-page">
-		<AddItem onAddItem={handleAddItem} />
-     
+      {/* flytta denna till rätt ställe (lägg till produkt) */}
+
+      {/* <AddItem onAddItem={handleAddItem} /> */}
+
       <img className="menu-hero-img" src={heroImg} alt="" />
       <div className="menu-grid">
         <SideMenu />
