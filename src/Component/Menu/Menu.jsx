9d486fd1ec4afe2e96d97c92589c563d268dcc1b
shweta <168186977+dharavi15@users.noreply.github.com>
2025-04-25 11:23:38 +0200

    removed comments

--- src/Component/Menu/Menu.jsx
+++ src/Component/Menu/Menu.jsx
@@ -1,61 +1,4 @@
-/*import React, { useEffect, useState } from "react";
-import MenuItem from "./MenuItem.jsx";
-import useCartStore from "../../data/cartStore.js";
-import { menuData } from "../../data/menuData.js";
 
-function Menu() {
-  const { foodDataList, totalPrice, toggleItemActive } = useCartStore();
-  const categories = ["Sushi", "Dumplings", "Snacks", "Drycker"];
-  const [editClick, setEditClick] = useState(false);
-
-  const handleEditClick = () => {
-    setEditClick((prev) => !prev);
-  };
-  
-  const [menuItems, setMenuItems] = useState(menuData);
-
-
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-  useEffect(() => {
-  const loginStatus = localStorage.getItem("isLoggedIn");
-  setIsLoggedIn(loginStatus === "true");
-}, []);
-
-
-  //För felsökning
-  useEffect(() => {
-    console.log(totalPrice);
-  }, [totalPrice]);
-
-  return (
-      <div className={`menu ${editClick ? "show-buttons" : ""}`}>
-  {isLoggedIn && (
-    <div className="add-edit-buttons">
-      <button className="edit-item-button" onClick={handleEditClick}>
-        {editClick ? "Klart" : "Redigera"}
-      </button>
-      <button className="add-item-button">Lägg till</button>
-    </div>
-  )}
-
-      {categories.map((category) => (
-        <div key={category}>
-          <h1 id={category}>{category}</h1>
-          {foodDataList
-            .filter((item) => item.category === category)
-            .map((foodItem) => (
-              <MenuItem key={foodItem.id} foodItem={foodItem} 
-              active={foodItem.active}/>
-            ))}
-        </div>
-      ))}
-    </div>
-  );
-}
-
-export default Menu;
-*/
 
 import React, { useEffect, useState } from "react";
 import MenuItem from "./MenuItem.jsx";
@@ -66,7 +9,7 @@ function Menu() {
   const { foodDataList, totalPrice, toggleItemActive } = useCartStore();
   const categories = ["Sushi", "Dumplings", "Snacks", "Drycker"];
   const [editClick, setEditClick] = useState(false);
-  const [isLoggedIn, setIsLoggedIn] = useState(null); // Start as null to avoid flickering
+  const [isLoggedIn, setIsLoggedIn] = useState(null); 
   const [menuItems, setMenuItems] = useState(menuData);
 
   useEffect(() => {
