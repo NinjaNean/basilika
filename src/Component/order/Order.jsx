dcef21d6181f22063f4f79ea7bd7a634a9101d94
Sally <sally1994lindgren@gmail.com>
2025-04-25 10:25:37 +0200

    style + function beställningsvy

--- src/Component/order/Order.jsx
+++ src/Component/order/Order.jsx
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import useCartStore from "../../data/cartStore.js";
 import "./Order.css";
+import minusImg from "../../assets/minus-icon-black.svg";
+import plusImg from "../../assets/plus-icon-black.svg";
 
 function Order() {
   const { cart, removeFromCart, addToCart, totalPrice, foodDataList } = useCartStore();
+  const [isOpen, setIsOpen] = useState(false);
 
   function findQuantity(id) {
     const quantity = cart.find((item) => item.id === id);
@@ -11,44 +14,53 @@ function Order() {
   }
 
   return (
-    <table className="cart">
-      <thead>
-        <tr>
-          <th>Antal</th>
-          <th>Produkt</th>
-          <th>Totalpris</th>
-        </tr>
-      </thead>
-      <tbody>
-        {cart.length === 0 ? (
-          <tr>
-            <td>Din kundvagn är tom</td>
-          </tr>
-        ) : (
-          foodDataList
-            .filter((item) => cart.find((cartItem) => cartItem.id === item.id))
-            .map((item) => (
-              <tr key={item.id}>
-                <td>
-                  <button onClick={() => removeFromCart(item)}>-</button>
-                  {findQuantity(item.id)}
-                  <button onClick={() => addToCart(item)}>+</button>
-                </td>
-                <td>
-                  <img src={item.img} alt="" /> {item.name} {item.price}:-
-                </td>
-                <td>{findQuantity(item.id) * item.price}:-</td>
-              </tr>
-            ))
-        )}
-      </tbody>
-      <tfoot>
-        <tr>
-          <th scope="row" colspan="2"></th>
-          <td>{totalPrice}:-</td>
-        </tr>
-      </tfoot>
-    </table>
+    <div className={`cart-wrapper ${isOpen ? "open" : ""}`} onClick={() => setIsOpen(!isOpen)}>
+      <button className="toggle-button" onClick={(e) => { e.        stopPropagation(); setIsOpen(!isOpen); }}>
+        {isOpen ? "Stäng" : "Se din beställning här!"}
+      </button>
+      <div className="cart" onClick={(e) => e.stopPropagation()}>
+        <div className="cart-header">
+          <p className="cart-heading">Antal</p>
+          <p className="cart-heading">Produkt</p>
+          <p className="cart-heading">Pris</p>
+        </div>
+
+        <div className="cart-body">
+          {cart.length === 0 ? (
+            <div className="cart-row">
+              <p>Du har inte lagt till något ännu!</p>
+            </div>
+          ) : (
+            foodDataList
+              .filter((item) => cart.find((cartItem) => cartItem.id === item.id))
+              .map((item) => (
+                <div className="cart-row" key={item.id}>
+                  <div className="cart-cell">
+                    <button onClick={(e) => { e.stopPropagation(); removeFromCart(item); }}>
+                      <img className="icon" src={minusImg} alt="minus icon" />
+                    </button>
+                    <span className="order-quantity">{findQuantity(item.id)}</span>
+                    <button onClick={(e) => { e.stopPropagation(); addToCart(item); }}>
+                      <img className="icon" src={plusImg} alt="plus icon" />
+                    </button>
+                  </div>
+                  <div className="cart-cell">
+                    <img className="order-img" src={item.img} alt="" /> {item.name} {item.price}:-
+                  </div>
+                  <div className="cart-cell">
+                    {findQuantity(item.id) * item.price}:-
+                  </div>
+                </div>
+              ))
+          )}
+        </div>
+
+        <div className="cart-footer">
+          <p className="cart-heading two">Totalpris</p>
+          <div className="total-price">{totalPrice}:-</div>
+        </div>
+      </div>
+    </div>
   );
 }
 
