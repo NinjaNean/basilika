a10ddf41f3be778fd7df9382991e068449de47ea
Linnéa Nilsson <linnea.nilsson97@hotmail.com>
2025-04-28 15:25:39 +0200

    responsiv + style

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -2,11 +2,7 @@ import React from "react";
 import InfoBox from "../Component/contactInfo/InfoBox";
 
 function Contact() {
-  return (
-    <div>
-      <InfoBox />
-    </div>
-  );
+  return <InfoBox />;
 }
 
 export default Contact;
